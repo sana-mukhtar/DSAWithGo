5398662997eda9ef728132eea3c5a13909fb2aff
Sana <sanamukhtar969@gmail.com>
2024-12-09 23:21:14 +0530

    implementCountDigits

--- 0.BasicsMaths/countDigits.go
+++ 0.BasicsMaths/countDigits.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+// Q.1 Given a positive integer n, count the number of digits in n that divide n evenly (i.e., without leaving a remainder). Return the total number of such digits.
+func countDigits(n int) int {
+	dividors := 0
+	copy := n
+	for n > 0 {
+		ld := n % 10
+		if copy%ld == 0 {
+			dividors++
+		}
+		n = n / 10
+	}
+	return dividors
+}
+
 func main() {
-	fmt.Println("helloe")
+	fmt.Println("hello")
+	fmt.Println(countDigits(3336999))
 }
