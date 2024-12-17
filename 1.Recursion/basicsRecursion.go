3ca490db3624c0f0cf8af9231f2403d150b10130
Sana <sanamukhtar969@gmail.com>
2024-12-17 23:37:52 +0530

    implement isPalindrome

--- 1.Recursion/basicsRecursion.go
+++ 1.Recursion/basicsRecursion.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
 
 // Print numbers from 1 to n without the help of loops. You only need to complete the function printNos() that takes n as a parameter and prints the number from 1 to n recursively.
 func printNos(i, num int) {
@@ -23,7 +27,7 @@ func printReverseNos(N int) {
 
 }
 
-//The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
 // F(0) = 0, F(1) = 1
 // F(n) = F(n - 1) + F(n - 2), for n > 1.
 // Given n, calculate F(n).
@@ -34,7 +38,32 @@ func fib(n int) int {
 	return fib(n-1) + fib(n-2)
 }
 
+// "Given a string, check if the string is palindrome or not."  A string is said to be palindrome if the reverse of the string is the same as the string.
+func isPalindrome(str string) bool {
+	var filteredStr string
+	for _, s := range str {
+		if unicode.IsLetter(s) {
+			filteredStr = filteredStr + string(s)
+		}
+	}
+	filteredStr = strings.ToLower(filteredStr)
+
+	len := len(strings.Split(filteredStr, ""))
+	left := 0
+	right := len - 1
+
+	for i := 0; i < len; i++ {
+		if filteredStr[left] != filteredStr[right] {
+			return false
+		}
+		left++
+		right--
+	}
+	return true
+}
+
 func main() {
 	// printNos(1, 10)
 	printReverseNos(5)
+	fmt.Println(isPalindrome("asdA:;,AD-Sa"))
 }
