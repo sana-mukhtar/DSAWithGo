222bc8d33af82eed2d75b47e83c681b1ffaec502
Sana <sanamukhtar969@gmail.com>
2024-12-28 00:15:18 +0530

    implement bubblesort

--- 3.Sorting/sort.go
+++ 3.Sorting/sort.go
@@ -9,7 +9,7 @@ func selectionSort(arr []int) []int {
 	for i := 0; i < len(arr); i++ {
 		lowestNum := arr[i]
 		track := i
-		for j := 0+i; j < len(arr); j++ {
+		for j := 0 + i; j < len(arr); j++ {
 			if arr[j] < lowestNum {
 				lowestNum = arr[j]
 				track = j
@@ -22,7 +22,21 @@ func selectionSort(arr []int) []int {
 	return arr
 }
 
+func bubbleSort(arr []int) []int {
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr)-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				temp := arr[j]
+				arr[j] = arr[j+1]
+				arr[j+1] = temp
+			}
+		}
+	}
+	return arr
+}
+
 func main() {
 	fmt.Println(selectionSort([]int{64, 25, 12, 22, 11}))
-	fmt.Println(selectionSort([]int{38, 27, 43, 3, 9, 82, 10}))
+	fmt.Println(bubbleSort([]int{38, 27, 43, 3, 9, 82, 10}))
+	fmt.Println(bubbleSort([]int{64, 25, 12, 22, 11}))
 }
