baafb870c815137b9cff9a2195d0033f7d448610
Sana <sanamukhtar969@gmail.com>
2024-12-27 21:59:24 +0530

    implement findLowestHighestFrequency func

--- 2.Hashmaps/maps.go
+++ 2.Hashmaps/maps.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func countElements(arr []int) map[int]int {
 	newMap := make(map[int]int)
@@ -10,8 +13,28 @@ func countElements(arr []int) map[int]int {
 	return newMap
 }
 
+// Find the highest/lowest frequency element
+func findLowestHighestFrequency(arr []int) (int, int) {
+	lowest, highest := math.MaxInt, math.MinInt
+	hashmap := countElements(arr)
+	lk, hk := 0, 0
+	for key, val := range hashmap {
+		if val > highest {
+			highest = val
+			hk = key
+		}
+		if val < lowest {
+			lowest = val
+			lk = key
+		}
+	}
+	return lk, hk
+}
+
 func main() {
-	arr := []int{2, 3, 5, 2, 4, 5, 1, 2, 9}
-	fmt.Println(countElements(arr))
+	arr := []int{2, 3, 3, 3, -1, 3, 2, 9, 9}
+	// fmt.Println(countElements(arr))
+	lowest, highest := findLowestHighestFrequency(arr)
+	fmt.Printf("lowest frequency number: %d highest frequency number: %d", lowest, highest)
 
 }
