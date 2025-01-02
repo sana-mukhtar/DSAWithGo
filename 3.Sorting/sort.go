6e0661488e89fa0e862e05d846223af2620c6aeb
Sana <sanamukhtar969@gmail.com>
2025-01-02 21:41:44 +0530

    implement insertion sort

--- 3.Sorting/sort.go
+++ 3.Sorting/sort.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 // Problem Statement: Given an array of N integers, write a program to implement the Selection sorting algorithm.
 func selectionSort(arr []int) []int {
@@ -35,8 +33,26 @@ func bubbleSort(arr []int) []int {
 	return arr
 }
 
+func insertionSort(arr []int) []int {
+	for i := 1; i < len(arr); i++ {
+		key := arr[i]
+		for j := i - 1; j >= 0 && key < arr[j]; j-- {
+			arr[j+1] = arr[j]
+			arr[j] = key
+		}
+	}
+	return arr
+}
+
+// 1 loop on array -> compare i and i+1-> if i+1 is smaller -> iterate inversely
+
 func main() {
-	fmt.Println(selectionSort([]int{64, 25, 12, 22, 11}))
-	fmt.Println(bubbleSort([]int{38, 27, 43, 3, 9, 82, 10}))
-	fmt.Println(bubbleSort([]int{64, 25, 12, 22, 11}))
+	// 	fmt.Println("selectionsort:", selectionSort([]int{64, 25, 12, 22, 11}))
+	// 	fmt.Println("bubblesort:", bubbleSort([]int{38, 27, 43, 3, 9, 82, 10}))
+	// 	fmt.Println("bubblesort:", bubbleSort([]int{64, 25, 12, 22, 11}))
+	fmt.Println("insertionsort:", insertionSort([]int{5, 4, 3, 2, 7}))
+	// for j := 0; j >= 0; j-- {
+	// 	println(j)
+	// }
+
 }
