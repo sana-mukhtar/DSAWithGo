09cf29cf709aa1d2bbb3273f765fdff5d5309156
Sana <sanamukhtar969@gmail.com>
2025-01-06 23:38:31 +0530

    implement isSorted

--- 4.Arrays/Basics/basics.go
+++ 4.Arrays/Basics/basics.go
@@ -27,8 +27,18 @@ func findSecondLargest(arr []int) int {
 	return secondLargest
 }
 
+func isSorted(arr []int) bool {
+	for i := 0; i < len(arr)-1; i++ {
+		if arr[i] > arr[i+1] {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
 	max := findLargest([]int{4, 52, 2, 78, 55})
 	fmt.Println(max)
 	fmt.Println(findSecondLargest([]int{3, 45, 32, 78, 12, 99, 100}))
+	fmt.Println(isSorted([]int{2, 3, 4, 5, 5, 6}))
 }
