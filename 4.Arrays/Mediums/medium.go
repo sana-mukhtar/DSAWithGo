c9c007c09b858ef8e69124cb6dc895131b40cd95
Sana <sanamukhtar969@gmail.com>
2025-06-09 23:58:23 +0530

    implement reArrangeArray

--- 4.Arrays/Mediums/medium.go
+++ 4.Arrays/Mediums/medium.go
@@ -214,7 +214,35 @@ func maxProfitBuyAndSellStock(prices []int) int {
 	return maxProfit
 }
 
+// You are given a 0-indexed integer array nums of even length consisting of an equal number of positive and negative integers.
+// You should return the array of nums such that the the array follows the given conditions:
+// Every consecutive pair of integers have opposite signs.
+// For all integers with the same sign, the order in which they were present in nums is preserved.
+// The rearranged array begins with a positive integer.
+// Return the modified array after rearranging the elements to satisfy the aforementioned conditions.
+// Input: nums = [3,1,-2,-5,2,-4]
+// Output: [3,-2,1,-5,2,-4]
+func rearrangeArray(nums []int) []int {
+	rearrangedArray, posNums, negNums := []int{}, []int{}, []int{}
+	for i := 0; i < len(nums); i++ {
+		if nums[i] > 0 {
+			posNums = append(posNums, nums[i])
+		}
+		if nums[i] < 0 {
+			negNums = append(negNums, nums[i])
+		}
+	}
+
+	for i := 0; i < len(nums)/2; i++ {
+		rearrangedArray = append(rearrangedArray, posNums[i])
+		rearrangedArray = append(rearrangedArray, negNums[i])
+	}
+
+	return rearrangedArray
+}
+
 func main() {
 	printMaxSubArrayUsingKadanesAlgo([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4})
 	fmt.Println(maxProfitBuyAndSellStock([]int{7, 1, 5, 3, 6, 4}))
+	fmt.Println(rearrangeArray([]int{3, 1, -2, -5, 2, -4}))
 }
