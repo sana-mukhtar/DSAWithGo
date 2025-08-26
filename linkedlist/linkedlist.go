ccf74b79b6a28660cbebce8b4ac0080a6fe11e02
Sana <sanamukhtar969@gmail.com>
2025-08-27 00:32:21 +0530

    implement logic to insert at head

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // The head stores the pointer to our first node. The length attribute stores the length of our linked list.
 type LinkedList struct {
 	head   *Node
@@ -10,4 +12,26 @@ type LinkedList struct {
 type Node struct {
 	data int
 	next *Node
+}
+
+func (ll *LinkedList) insertAtHead(data int) {
+	newNode := Node{
+		data: data,
+		next: nil,
+	}
+
+	if ll.head == nil {
+		ll.head = &newNode
+	} else {
+		orgNode := ll.head
+		ll.head = &newNode
+		newNode.next = orgNode
+	}
+	ll.length++
+}
+
+func main() {
+	// We can initialize our linked list this way. The initial length is zero because there are no nodes in our list yet. The head points to nil because there are no nodes to point to.
+	list := LinkedList{nil, 0}
+	fmt.Println(list)
 }
