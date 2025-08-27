b7ab7a4b539fabc2d0ab8594c7111ebce3dfd83d
Sana <sanamukhtar969@gmail.com>
2025-08-27 23:21:32 +0530

    implement insertAtTail

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -14,6 +14,7 @@ type Node struct {
 	next *Node
 }
 
+// insertAtHead accepts data to add in linkedList chain. If the LL is empty it inserts the data to head otherwise store the head node to temporary variable and add the provided data to the head then points head's next node to the temporary head variable.
 func (ll *LinkedList) insertAtHead(data int) {
 	newNode := Node{
 		data: data,
@@ -30,8 +31,39 @@ func (ll *LinkedList) insertAtHead(data int) {
 	ll.length++
 }
 
+func (ll *LinkedList) insertAtTail(data int) {
+	newNode := Node{
+		data: data,
+		next: nil,
+	}
+
+	if ll.head == nil {
+		ll.head = &newNode
+	} else {
+		existingNode := ll.head
+		for existingNode.next != nil {
+			existingNode = existingNode.next
+		}
+		existingNode.next = &newNode
+	}
+
+	ll.length++
+}
+
+//  insert data at an arbitrary location.
+func (ll *LinkedList) insert(data int) {
+}
+
 func main() {
 	// We can initialize our linked list this way. The initial length is zero because there are no nodes in our list yet. The head points to nil because there are no nodes to point to.
 	list := LinkedList{nil, 0}
-	fmt.Println(list)
+
+	for i := 0; i < 5; i++ {
+		if i == 0 {
+			fmt.Println(list)
+		}
+		list.insertAtHead(i)
+		fmt.Println(list)
+	}
+
 }
