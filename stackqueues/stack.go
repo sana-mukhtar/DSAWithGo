e586e15af7c5b9a800c53bcba6b76102321c947e
Sana <sanamukhtar969@gmail.com>
2026-05-03 16:24:09 +0530

    implement queue

--- stackqueues/stack.go
+++ stackqueues/stack.go
@@ -54,4 +54,67 @@ func main() {
 	fmt.Println(st.pop())
 
 	fmt.Println("stack", st.elements)
+
+	q := newQueue()
+	q.enqueue(1)
+	q.enqueue(2)
+	q.enqueue(3)
+	q.enqueue(4)
+	q.enqueue(5)
+	fmt.Println("queue", q.elements)
+
+	fmt.Println("dequeue", q.dequeue())
+	fmt.Println("dequeue", q.dequeue())
+	fmt.Println("dequeue", q.dequeue())
+	fmt.Println("q elems", q.elements)
+
+	fmt.Println("front", q.front())
+	fmt.Println("rear", q.rear())
+	fmt.Println(q.elements)
+}
+
+type queue struct {
+	elements []int
+}
+
+func newQueue() *queue {
+	return &queue{
+		elements: make([]int, 0),
+	}
+}
+
+func (q *queue) enqueue(elem int) {
+	q.elements = append(q.elements, elem)
+}
+
+func (q *queue) dequeue() int {
+	if len(q.elements) == 0 {
+		panic("queue is already empty")
+	}
+
+	elemToBeRemoved := q.elements[0]
+	q.elements = q.elements[1:]
+
+	return elemToBeRemoved
+}
+
+func (q *queue) front() int {
+	if len(q.elements) == 0 {
+		panic("queue is empty")
+	}
+
+	return q.elements[0]
+}
+
+func (q *queue) rear() int {
+
+	if len(q.elements) == 0 {
+		panic("queue is empty")
+	}
+
+	return q.elements[len(q.elements)-1]
+}
+
+func (q *queue) size(elem int) int {
+	return len(q.elements)
 }
