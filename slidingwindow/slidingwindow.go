858453a5785f1e4489ecb4a188efb5992eafff03
Sana <sanamukhtar969@gmail.com>
2026-05-19 23:32:59 +0530

    implement count anagram occurence

--- slidingwindow/slidingwindow.go
+++ slidingwindow/slidingwindow.go
@@ -74,7 +74,60 @@ func firstNegativeOnEachWindow(arr []int, windowSize int) []int {
 	return result
 }
 
+func countAnagramOccurences(str, ptrn string) int {
+	n, k := len(str), len(ptrn)
+
+	if n < k {
+		return 0
+	}
+
+	count := 0
+	patternFreq, windowFreq := make(map[byte]int), make(map[byte]int)
+
+	// Store pattern frequency
+	for i := 0; i < k; i++ {
+		patternFreq[ptrn[i]]++
+	}
+
+	// calculate first window
+	for i := 0; i < k; i++ {
+		windowFreq[str[i]]++
+	}
+
+	// check if first window is anagram
+	if isAnagram(patternFreq, windowFreq) {
+		count++
+	}
+
+	for i := k; i < n; i++ {
+		// Remove left character
+		windowFreq[str[i-k]]--
+
+		// Add new right character
+		windowFreq[str[i]]++
+
+		// 2. Do calculation on current window
+		if isAnagram(patternFreq, windowFreq) {
+			count++
+		}
+	}
+
+	return count
+}
+
+func isAnagram(a, b map[byte]int) bool {
+	for ch, freq := range a {
+		if b[ch] != freq {
+			return false
+		}
+	}
+
+	return true
+}
+
 func main() {
 	fmt.Println("Start small. Ship something.", maxSubArraySum([]int{2, 1, 5, 1, 3, 2}, 3))
 	fmt.Println("negatives in each window", firstNegativeOnEachWindow([]int{-1, -2, 1, 2, 3, -5, 6, -8}, 3))
+	fmt.Println("anagram occurences", countAnagramOccurences("forxxorfxdofr", "for"))
+	fmt.Println("anagram occurences", countAnagramOccurences("aabaababaa", "abaa"))
 }
