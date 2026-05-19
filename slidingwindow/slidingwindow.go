0fcd416517e99b10c30001f521fefa7e6910cb26
Sana <sanamukhtar969@gmail.com>
2026-05-19 21:16:24 +0530

    add comment on maxSubArray sum

--- slidingwindow/slidingwindow.go
+++ slidingwindow/slidingwindow.go
@@ -2,29 +2,33 @@ package main
 
 import "fmt"
 
-func maxSubArraySum(arr []int, k int)int{
-    n := len(arr)
-    
-    if n <k {
-        return -1
-    }
-    
-    windowSum:=0
-    for i :=0; i<k; i++{
-        windowSum += arr[i]
-    }
-    
-    maxSum := windowSum
+func maxSubArraySum(arr []int, k int) int {
+	n := len(arr)
+
+	if n < k {
+		return -1
+	}
+
+	windowSum := 0
+	// Reach till the first fixed window of size k
+	for i := 0; i < k; i++ {
+		windowSum += arr[i]
+	}
+
+	maxSum := windowSum
 	for i := k; i < n; i++ {
-        windowSum = windowSum-arr[i-k]+arr[i]
-        
-        if windowSum > maxSum {
-            maxSum = windowSum
-        }
-    }
-    return maxSum
+		// slide the window
+		windowSum = windowSum - arr[i-k] + arr[i]
+
+		// Do calculation on current window
+		if windowSum > maxSum {
+			maxSum = windowSum
+		}
+	}
+
+	return maxSum
 }
 
 func main() {
-  fmt.Println("Start small. Ship something.", maxSubArraySum([]int{2, 1, 5, 1, 3, 2}, 3))
+	fmt.Println("Start small. Ship something.", maxSubArraySum([]int{2, 1, 5, 1, 3, 2}, 3))
 }
